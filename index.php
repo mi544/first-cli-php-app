6f38b77becf5c1e66dcbee01dbf79c26dd422e8b
Maksim Verkhoturov <sd32@protonmail.com>
2021-01-24 18:56:44 -0700

    moves veggies array into separate file

--- index.php
+++ index.php
@@ -1,19 +1,12 @@
 <?php
-echo "Hello!\nWelcome!\n\n";
 function askHungry($question)
 {
-    $veggies = [
-        "potato",
-        "cucumber",
-        "bunch of kale leaves",
-        "tofu in blocks",
-        "miso soup",
-    ];
     $isHungry = readline($question . '(y/n): ');
     if (strtolower(trim($isHungry)) == 'n') {
         echo "Have a good day!";
         exit;
     } else if (strtolower(trim($isHungry)) == 'y') {
+        require 'veggies.php';
         $randomVeggie = $veggies[random_int(0, sizeof($veggies) - 1)];
         echo "Eat this $randomVeggie! Have a good day!\n\n";
         askHungry("Are you still hungry?");
@@ -22,4 +15,6 @@ function askHungry($question)
         askHungry("Are you hungry?");
     }
 }
+
+echo "Hello!\nWelcome!\n\n";
 askHungry("Are you hungry?");
---
+++ veggies.php
@@ -0,0 +1,9 @@
+<?php
+
+$veggies = [
+    "potato",
+    "cucumber",
+    "bunch of kale leaves",
+    "tofu in blocks",
+    "miso soup",
+];
